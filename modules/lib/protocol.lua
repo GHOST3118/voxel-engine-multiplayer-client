1c3a207cf34a79d25568a720b57cf0a40adf680d
Murka124 <67805801+Murka124@users.noreply.github.com>
2025-02-01 02:41:48 +0300

    protocol enhancement
    добавлена поддержка структур и массивов в протоколе для декодирования. структуры можно задавать в protocol.json и использовать там же, массивам можно просто указать тип данных.
    кодирование таких типов пока не реализовано.

--- modules/lib/protocol.lua
+++ modules/lib/protocol.lua
@@ -31,6 +31,10 @@ protocol.slice_table = function(tbl, first, last, step)
     return sliced
 end
 
+-- чтобы функции были доступны из DATA_DECODE
+local recursive_parse = function (data_struct, buffer, result) end
+local data_decode = function (data_type, buffer) end
+
 -- Функции для кодирования и декодирования разных типов значений
 local DATA_ENCODE = {
     ["boolean"] = function(buffer, value)
@@ -86,12 +90,16 @@ local DATA_ENCODE = {
         for i = 1, #value, 1 do
             buffer:pack_string(value[i])
         end
-    end -- сначала кодируем количество строк, потом сами строки
+    end
+    -- TODO: structure и array
 }
 
 local DATA_DECODE = {
     ["boolean"] = function(buffer)
         return buffer:get_bool()
+    end,
+    ["byte"] = function (buffer)
+        return buffer:get_byte()
     end,
     ["int8"] = function(buffer)
         return buffer:get_byte()
@@ -115,10 +123,10 @@ local DATA_DECODE = {
     ["int64"] = function(buffer)
         local result = bincode.decode_varint(buffer)
         return bincode.zigzag_decode(result)
-    end, -- TODO: byteutil.unpack как выше
+    end,
     ["uint64"] = function(buffer)
         return bincode.decode_varint(buffer)
-    end, -- здесь тоже
+    end,
     ["float"] = function(buffer)
         return buffer:get_float32()
     end,
@@ -135,54 +143,56 @@ local DATA_DECODE = {
         local string = unpack_string(buffer)
         return string
     end,
-    ["byteArray"] = function(buffer)
-        local length = bincode.decode_varint(buffer)
-        local bytes = buffer:get_bytes(length)
-        return bytes
-    end,
-    ["block_vec"] = function(buffer)
-        local DATA_DECODE = {
-            ["int32"] = function(buffer)
-                local result = bincode.decode_varint(buffer)
-                return bincode.zigzag_decode(result)
-            end,
-            ["uint8"] = function(buffer)
-                return bincode.decode_varint(buffer)
-            end,
-            ["uint16"] = function(buffer)
-                return bincode.decode_varint(buffer)
-            end
-        }
+    ["structure"] = function (buffer, struct_name)
+        local result = {}
+        local struct_index = 0
+        -- узнаём индекс нужной структуры
+        for index, structure in ipairs(protocol.Structures) do
+            if structure[1] == struct_name then struct_index = index break end end
 
-        --buffer:set_position(1)
-        local element_count = bincode.decode_varint(buffer)
-
-        if element_count == 0 then return {} end
-        local elements = {}
-        for i = 1, element_count, 1 do
-            local element = {}
-            local __table = protocol.data.structures[protocol.Structures.Block] or {}
-            for key, value in pairs(__table) do
-                if key ~= 1 then
-                    element[string.explode(":", value)[1]] = DATA_DECODE[string.explode(":", value)[2]](buffer)
-                end
-            end
-
-            table.insert(elements, element)
+        local vec_length = bincode.decode_varint(buffer)
+        -- TODO: уточнить у вампала, кодируется ли количество элементов Vec<T> в varint или leb128
+        -- но пока не ёбнет пусть будет varint
+        for i = 1, vec_length, 1 do
+            result[i] = {}
+            recursive_parse(protocol.Structures[struct_index], buffer, result[i])
         end
 
-        return elements
+        return result
     end,
-    ["stringArray"] = function(buffer)
-        local element_count = bincode.decode_varint(buffer)
-        local strings = {}
-        for i = 1, element_count, 1 do
-            strings[i] = buffer:unpack_string()
+    ["array"] = function (buffer, data_type)
+        local result = {}
+        local array_length = bincode.decode_varint(buffer)
+        for i = 1, array_length, 1 do
+            result[#result+1] = data_decode(data_type, buffer)
         end
-        return strings
-
+        return result
     end
 }
+
+---Помощник для DATA_DECODE[array].
+---Создан для доступа к своим же элементам. Объявлен выше ↑
+---@param data_type string Строка, в которой закодирован тип данных
+---@param buffer table Буфер
+---@return string|number|table|unknown
+data_decode = function (data_type, buffer)
+    return DATA_DECODE[data_type](buffer)
+end
+
+---Помощник парсера пакетов. Используется в DATA_ENCODE
+---и protocol.parse_packet. Позволяет парсить структуры. Объявлен выше ↑
+---@param data_struct table Структура данных (может содержать другие структуры)
+---@param buffer table Буфер
+---@param result table Таблица, в которой будут записаны данные
+recursive_parse = function(data_struct, buffer, result)
+    for key, value in pairs(data_struct) do
+        if key ~= 1 then
+            local type_descr = string.explode("|", value)[1]
+            local struct_descr = string.explode("|", value)[2]
+            result[string.explode(":", type_descr)[1]] = DATA_DECODE[string.explode(":", type_descr)[2]](buffer, struct_descr)
+        end
+    end
+end
 
 ---Создаёт датабуфер с порядком Big Endian
 ---@param bytes table|nil [Опционально] Таблица с байтами
@@ -240,24 +250,6 @@ function protocol.create_databuffer(bytes)
         self:set_order("LE")
     end
 
-    -- распутываем порядок байт если вдруг в движке перепутан (для работоспособности в 0.25)
-    -- TODO: вырезать распутыватель LE/BE, если 0.25 не поддерживается
-    -- local testbuf = data_buffer:new()
-    -- testbuf:set_order("LE")
-    -- testbuf:put_uint16(1)
-    -- if testbuf.bytes[2] == 1 then
-    --     buf.wrong_set_order = buf.set_order
-    --     function buf:set_order(str)
-    --         str = utf8.upper(str)
-    --         if str == "BE" then
-    --             self:wrong_set_order("LE")
-    --         else
-    --             self:wrong_set_order("BE")
-    --         end
-    --     end
-    --     buf:set_order(protocol.data.order)
-    -- end
-
     return buf
 end
 
@@ -270,6 +262,7 @@ function protocol.build_packet(client_or_server, packet_type, ...)
     local args = {...}
     local buffer = protocol.create_databuffer()
     buffer:put_byte(packet_type - 1)
+    -- TODO: поддержка structure и array
     for key, value in pairs(protocol.data[client_or_server][packet_type]) do
         if key > 1 then
             DATA_ENCODE[string.explode(":", value)[2]](buffer, args[key - 1])
@@ -289,12 +282,10 @@ function protocol.parse_packet(client_or_server, data)
     buffer:set_position(1) -- движок поставит позицию в конец буфера, возвращаем обратно в начало
     local packet_type = buffer:get_byte() + 1
     result.packet_type = packet_type
-    local __table = protocol.data[client_or_server][packet_type] or {}
-    for key, value in pairs(__table) do
-        if key ~= 1 then
-            result[string.explode(":", value)[1]] = DATA_DECODE[string.explode(":", value)[2]](buffer)
-        end
-    end
+    local data_struct = protocol.data[client_or_server][packet_type] or {}
+    -- TODO: улучшить парсинг для возможности парсинга структур (сделано) и массивов (сделано)
+    recursive_parse(data_struct, buffer, result)
+
     return result
 end
 
@@ -315,26 +306,27 @@ function protocol.check_packet(client_or_server, packet)
     return true
 end
 
-function protocol.parse_array_of(structure, data)
-    local elements = {}
-    local buffer = data_buffer()
+-- закомментировал лишнее. если что-нибудь без этого ёбнет, раскомментировать.
+-- function protocol.parse_array_of(structure, data)
+--     local elements = {}
+--     local buffer = data_buffer()
 
-    buffer:put_bytes(data)
-    buffer:set_position(1)
+--     buffer:put_bytes(data)
+--     buffer:set_position(1)
 
-    while buffer.pos < buffer:size() do
-        local element = {}
+--     while buffer.pos < buffer:size() do
+--         local element = {}
 
-        local __table = protocol.data.structures[structure] or {}
-        for key, value in pairs(__table) do
-            if key ~= 1 then
-                element[string.explode(":", value)[1]] = DATA_DECODE[string.explode(":", value)[2]](buffer)
-            end
-        end
-    end
+--         local __table = protocol.data.structures[structure] or {}
+--         for key, value in pairs(__table) do
+--             if key ~= 1 then
+--                 element[string.explode(":", value)[1]] = DATA_DECODE[string.explode(":", value)[2]](buffer)
+--             end
+--         end
+--     end
 
-    return elements
-end
+--     return elements
+-- end
 
 -- Перечисление сообщений клиента
 protocol.ClientMsg = {}
@@ -359,11 +351,10 @@ for index, value in ipairs(protocol.data.states) do
     protocol.States[index] = value
     protocol.States[value] = index
 end
-
 -- Парсим из json Структуры
 for index, value in ipairs(protocol.data.structures) do
-    protocol.Structures[index] = value[1]
-    protocol.Structures[value[1]] = index
+    protocol.Structures[index] = value
+    protocol.Structures[value] = index
 end
 
 -- выставляем в свет функцию leb128_чего-то-там, но теперь функция возвращает таблицу с байтами и принимает
