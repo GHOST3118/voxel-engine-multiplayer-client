74062f3a7a3f7d66d70995e778cc0bd8b9b698cf
Xertis <118364459+Xertis@users.noreply.github.com>
2025-03-08 00:07:18 +0300

    api fixes

--- modules/api/events.lua
+++ modules/api/events.lua
@@ -1,19 +1,17 @@
 local protocol = require "lib/protocol"
-local List = require "lib/common/list"
-local ClientQueue = require "multiplayer/client/client_queue"
 
 local module = {}
 local handlers = {}
 
 function module.send(pack, event, bytes)
-    local buffer = protocol.create_databuffer()
-    buffer:put_packet(protocol.build_packet("client", protocol.ClientMsg.PackEvent, pack, event, bytes))
-    List.pushright(ClientQueue, buffer.bytes)
+    local packet = protocol.build_packet("client", protocol.ClientMsg.PackEvent, pack, event, bytes)
+
+    Session.client:push_packet(packet)
 end
 
 function module.on(pack, event, func)
     local pack_handlers = table.set_default(handlers, pack, {})
-    local pack_handler_events = table.set_default(pack_handlers, event {})
+    local pack_handler_events = table.set_default(pack_handlers, event, {})
 
     table.insert(pack_handler_events, func)
 end
