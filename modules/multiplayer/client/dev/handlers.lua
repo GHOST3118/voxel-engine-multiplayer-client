ffcb4687b85e68acf8237a34fe5dcb87fbc6331a
Xertis <118364459+Xertis@users.noreply.github.com>
2025-03-08 18:06:27 +0300

    chunks responce optimizate

--- modules/multiplayer/client/dev/handlers.lua
+++ modules/multiplayer/client/dev/handlers.lua
@@ -11,6 +11,12 @@ ClientHandlers[ protocol.ServerMsg.ChunkData ] = function (packet)
     world.set_chunk_data(packet.x, packet.z, Bytearray(packet.data), true)
 end
 
+ClientHandlers[ protocol.ServerMsg.ChunksData ] = function (packet)
+    for _, chunk in ipairs(packet.list) do
+        world.set_chunk_data(chunk.x, chunk.z, Bytearray(chunk.data), true)
+    end
+end
+
 ClientHandlers[ protocol.ServerMsg.PackEvent ] = function (packet)
     api_events.__emit__(packet.pack, packet.event, packet.bytes)
 end
