711ddec497479a8d8a3f0bf5fcb6cbdb622fc78c
Xertis <118364459+Xertis@users.noreply.github.com>
2025-06-05 15:12:38 +0300

    простите, я баг пофиксил

--- modules/api/entities.lua
+++ modules/api/entities.lua
@@ -4,32 +4,37 @@ local module = {}
 local entities_uids = {}
 local entities_components = {}
 local handlers = {}
-local desyns_entities = {}
+local desynced_entities = {}
 
-local spawn = entities.spawn
+local function vec_zero()
+    return {0, 0, 0}
+end
 
-function entities.spawn(name, ...)
-    local prefix = parse_path(debug.getinfo(2).source)
-    if table.has(desyns_entities, name) or prefix == "multiplayer" then
-        return spawn(name, ...)
+local original_spawn = entities.spawn
+
+entities.spawn = function(name, ...)
+    local source_path = debug.getinfo(2, "S").source
+    local prefix = parse_path(source_path) or ""
+
+    if desynced_entities[name] or prefix:find("multiplayer") then
+        return original_spawn(name, ...)
     end
 
-    local entity = spawn(name, ...)
+    local entity = original_spawn(name, ...)
     entity:despawn()
 
     local packet = protocol.build_packet("client", protocol.ClientMsg.EntitySpawnTry, entity:def_index(), {...})
-
     Session.client:push_packet(packet)
 
     return entity
 end
 
 function module.desync(name)
-    table.insert(desyns_entities, name)
+    desynced_entities[name] = true
 end
 
 function module.sync(name)
-    table.remove_value(desyns_entities, name)
+    desynced_entities[name] = nil
 end
 
 function module.set_handler(triggers, handler)
@@ -42,23 +47,29 @@ function module.__despawn__(uid)
     local cuid = entities_uids[uid]
     if not cuid then return end
 
+    entities_uids[uid] = nil
     local entity = entities.get(cuid)
     if entity then
         entity:despawn()
-        entities_uids[uid] = nil
     end
 end
+
 
 function module.__get_uids__()
     return entities_uids
 end
 
 function module.__emit__(uid, def, dirty)
+    local std_fields = dirty.standart_fields or {}
 
     if not entities_uids[uid] then
-        local centity = spawn(entities.def_name(def), {0, 0, 0})
-        entities_uids[uid] = centity:get_uid()
-        centity.rigidbody:set_gravity_scale({0, 0, 0})
+        local entity_name = entities.def_name(def)
+        local new_entity = original_spawn(entity_name, std_fields.tsf_pos or vec_zero())
+        entities_uids[uid] = new_entity:get_uid()
+
+        if new_entity.rigidbody then
+            new_entity.rigidbody:set_gravity_scale(vec_zero())
+        end
     end
 
     local cuid = entities_uids[uid]
@@ -66,63 +77,68 @@ function module.__emit__(uid, def, dirty)
     if not entity then return end
 
     if handlers[def] then
-        handlers[def](cuid, def, dirty.custom_fields)
+        handlers[def](cuid, def, dirty.custom_fields or {})
     end
 
-    local standard_fields = dirty.standart_fields or {}
-    local textures = dirty.textures or {}
-    local components = dirty.components or {}
-    local models = dirty.models or {}
-    local tsf, body, rig = entity.transform, entity.rigidbody, entity.skeleton
+    if dirty.components then
+        for comp_name, enabled in pairs(dirty.components) do
+            local comp = entity:get_component(comp_name)
+            if not comp then goto continue end
 
-    for key, val in pairs(textures) do
-        rig:set_texture(key, val)
-    end
+            entities_components[cuid] = entities_components[cuid] or {}
+            local comp_cache = entities_components[cuid][comp_name] or {}
 
-    for key, val in pairs(models) do
-        rig:set_model(tonumber(key), val)
-    end
-
-    for component, val in pairs(components) do
-        local comp_tbl = entity:get_component(component)
-        if not val then
-            table.set_default(entities_components, cuid, {})
-            if comp_tbl then
-                for key, func in pairs(comp_tbl) do
-                    if type(func) == "function" then
-                        entities_components[cuid][key] = func
-                        comp_tbl[key] = function() end
+            if enabled then
+                for fn_name, fn in pairs(comp_cache) do
+                    comp[fn_name] = fn
+                end
+                entities_components[cuid][comp_name] = nil
+            else
+                entities_components[cuid][comp_name] = {}
+                for fn_name, fn in pairs(comp) do
+                    if type(fn) == "function" then
+                        comp_cache[fn_name] = fn
+                        comp[fn_name] = function() end
                     end
                 end
             end
-        else
-            local comp_func = table.set_default(entities_components, cuid, {})
-            for key, func in pairs(comp_func) do
-                comp_tbl[key] = func
-            end
+            ::continue::
         end
     end
 
-    if standard_fields.tsf_pos then
-        local cur_pos = tsf:get_pos()
-        local target_pos = standard_fields.tsf_pos
-        local direction = vec3.sub(target_pos, cur_pos)
+    local skeleton = entity.skeleton
+    if skeleton then
+        for key, val in pairs(dirty.textures or {}) do
+            skeleton:set_texture(key, val)
+        end
+        for key, val in pairs(dirty.models or {}) do
+            skeleton:set_model(tonumber(key), val)
+        end
+    end
+
+
+    local transform = entity.transform
+    local rigidbody = entity.rigidbody
+
+    if std_fields.tsf_pos then
+        local current_pos = transform:get_pos()
+        local target_pos = std_fields.tsf_pos
+        local direction = vec3.sub(target_pos, current_pos)
         local distance = vec3.length(direction)
 
-        if distance < 0.01 or distance > 10 then
-            tsf:set_pos(target_pos)
-            body:set_vel({0, 0, 0})
-        else
+        if distance > 10 or distance < 0.01 then
+            transform:set_pos(target_pos)
+            rigidbody:set_vel(vec_zero())
+        elseif rigidbody then
             local time_to_reach = 0.1
-            local speed = distance / time_to_reach
-            local velocity = vec3.mul(vec3.normalize(direction), speed)
-            body:set_vel(velocity)
+            local velocity = vec3.mul(vec3.normalize(direction), distance / time_to_reach)
+            rigidbody:set_vel(velocity)
         end
     end
 
-    if standard_fields.tsf_rot then tsf:set_rot(standard_fields.tsf_rot) end
-    if standard_fields.tsf_size then tsf:set_size(standard_fields.tsf_size) end
-    if standard_fields.body_size then body:set_size(standard_fields.body_size) end
+    if std_fields.tsf_rot then transform:set_rot(std_fields.tsf_rot) end
+    if std_fields.tsf_scale then transform:set_scale(std_fields.tsf_scale) end
+    if std_fields.body_size then rigidbody:set_size(std_fields.body_size) end
 end
 
 return module
